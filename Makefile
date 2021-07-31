12874f2146f27059d6d78eac0a680b5a5155db07
nhatvu148 <nhatvu148@gmail.com>
2021-08-01 07:00:02 +0900

    add route guide

--- Makefile
+++ Makefile
@@ -5,3 +5,9 @@ server:
 	
 client:
 	cargo run --bin helloworld-client
+
+server1:
+	cargo run --bin routeguide-server
+	
+client1:
+	cargo run --bin routeguide-client
--- build.rs
+++ build.rs
@@ -2,4 +2,9 @@ fn main() {
     tonic_build::configure()
         .compile(&["proto/helloworld.proto"], &["proto"])
         .unwrap();
+
+    tonic_build::configure()
+        .type_attribute("routeguide.Point", "#[derive(Hash)]")
+        .compile(&["proto/route_guide.proto"], &["proto"])
+        .unwrap();
 }
